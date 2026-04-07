ff2392d2a5899d3e1baa2922a2c142bba4d40100
Vedika Apte <vedikaapte.work@gmail.com>
2026-04-07 22:52:08 +0530

    Update refractive_index.py

--- Optics/Refractive Index/refractive_index.py
+++ Optics/Refractive Index/refractive_index.py
@@ -1,1 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+python program to find refractive index using defind function
 
+@author: vedika_apte
+"""
+
+# Calculate refractive index 
+
+def Refractive_Index(c,v):
+    return (c/v)
+
+try:
+    vel = input("Enter velocity(m/s):")
+    v = float(vel)
+    
+    c = 3.0
+    
+    if v==0:
+        print("Velocity cannot be zero")
+    else:
+        n = Refractive_Index(c,v)
+        print("Refractive Index =" , n)
+
+except:
+    print("Error: Enter a numeric value")
+    
+Refractive_Index(c,v)
